ae457775f276124044ec8903c8f4b7c5cbb59a60
Luiz Michel <michelwork18@hotmail.com>
2025-02-10 19:16:46 -0300

    atualizando aplicação

--- Palindromo.py
+++ Palindromo.py
@@ -1,30 +1,63 @@
-# 1 - Solução
-# txt = input("type the word you want to check if it is a palindrome:")
-
-# txtInvert = txt[::-1]
-
-# if txt == txtInvert:
-#     print("Is a Palindromo")
-# else:
-#     print("Is not a Palindromo")
-
-# 2 - Solução
-palavrainput = "renner"
-# comprimento = len(palavrainput)
+def stringForList(str):
+    newList = list()
+    for ch in str:
+        newList.append(ch)
+    return newList
 
 
-a = list()
-b = list()
-for ch in palavrainput:
-    a.append(ch)
-for ch in reversed(a):
-    b.append(ch)
+def mySorted(list):
+    tamanho = len(list)
+    for i in range(tamanho):
+        for j in range(tamanho -1 -i):
+            if list[j] > list[j + 1]:
+                temp = list[j]
+                list[j] = list[j + 1]
+                list[j + 1] = temp
+    return list
 
-print(a)
-print(b)
+def listForString(list):
+    ret = ""
+    for i in range(len(list)):
+        ret = ret + list[i]
+    
+    return ret
 
-if a == b:
-    print("é faz um palindromo")
-else:
-    print("não é um palindromo")
+
+def palindromo(txt):
+    a = list()
+    b = list()
+    for ch in txt:
+        a.append(ch)
+    for ch in reversed(txt):
+        b.append(ch)
+    if a.upper() == b.upper():
+        print("é faz um palindromo")
+    else:
+        print("não é um palindromo")
+
+
+
+
+def anagram(str1, str2):
+    l = stringForList(str1)
+    ll = stringForList(str2)
+    ret = mySorted(l)
+    rett = mySorted(ll)
+    retFinal = listForString(ret)
+    retFinall = listForString(rett)
+    if retFinal.upper() == retFinall.upper():
+        print(str1 + " " + str2 +" ::::: As Strings são Anagramas!")
+    else:
+        print(str1 + " " + str2 +" ::::: NÃO São Anagramas!")
+
+
+def anagramDescomplicado(str1,str2):
+    ret = sorted(str1)
+    rett = sorted(str2)
+    ret = "".join(ret)
+    rett = "".join(rett)
+    if ret.upper() == rett.upper():
+        print(str1 + " " + str2 +" ::: São Anagramas!")
+    else:
+        print(str1 + " " + str2 +" NÃO São Anagramas!")
 
